a93e44ce53fd2deda2e4494741634b75a3851e47
CWWhitney <whitney.cory@gmail.com>
2025-01-11 21:57:32 +0700

    move functions

--- hill_climbing.R
+++
@@ -1,9 +0,0 @@
-hc <- function (x, start = NULL, whitelist = NULL, blacklist = NULL,
-                score = NULL, ..., debug = FALSE, restart = 0, perturb = 1,
-                max.iter = Inf, maxp = Inf, optimized = TRUE)
-{
-  greedy.search(x = x, start = start, whitelist = whitelist,
-                blacklist = blacklist, score = score, heuristic = "hc",
-                debug = debug, ..., restart = restart, perturb = perturb,
-                max.iter = max.iter, maxp = maxp, optimized = optimized)
-}
